7704dcba1027948d458b963af2eeb80378454944
ruebezashi <73941930+ruebezashi@users.noreply.github.com>
2021-02-16 18:05:38 +0100

    Update Pk.py
    Updated Pk.py for use on COSMA

--- Pk.py
+++ Pk.py
@@ -9,18 +9,15 @@ num_snaps = int(input("Please enter the number of snapshots you wish to analyse 
 #creates a list containing the paths to the snapshots
 snapshots = [None]*(num_snaps)
 
-#basic path where all files are placed, preliminarily set to Julian's nucome folder on S3IT
-path = "/data/jadame/nucome/0.0eV/"
+#basic path where all files are placed
+path = "/madfs/nucome/"
 
 #the path to the reference run
 ref = input("Please enter the path to the reference run that the snapshots are compared to \n" + path)
 
-#snapshot_name = "test1024hybrid_snap005_cdm"
-#snapshot1 = '/data/jadame/nucome/0.0eV/output/' + snapshot_name  #snapshot name
-#snapshot2 = "/data/jadame/nucome/0.0eV/snapdir_005/snap_005"
 
-grid     = 128                    #grid size, chose a small one for now because of RAM issues
-ptypes   = [1,2]                   #CDM + neutrinos
+grid     = 256                     #grid size
+ptypes   = [1]                     #we only are interested in the CDM + baryon spectrum
 MAS      = 'CIC'                   #Cloud-in-Cell
 do_RSD   = False                   #dont do redshif-space distortions
 axis     = 0                       #axis along which place RSD; not used here
@@ -78,71 +75,3 @@ for i in range(num_snaps):
 
 plt.subplots_adjust(wspace = 0.4, hspace = 0.2)
 fig.savefig("analysis.pdf")
-# Compute the effective number of particles/mass in each voxel
-#delta1 = MASL.density_field_gadget(snapshot1, ptypes, grid, MAS, do_RSD, axis, verbose)
-#delta2 = MASL.density_field_gadget(snapshot2, ptypes, grid, MAS, do_RSD, axis, verbose)
-
-# compute density contrast: delta = rho/<rho> - 1
-#delta1 /= np.mean(delta1, dtype=np.float64);  delta1 -= 1.0
-#delta2 /= np.mean(delta2, dtype=np.float64);  delta2 -= 1.0
-
-
-# compute power spectrum
-#Pk1 = PKL.Pk(delta1, BoxSize, axis, MAS, threads, verbose)
-#Pk2 = PKL.Pk(delta2, BoxSize, axis, MAS, threads, verbose)
-# Pk is a python class containing the 1D, 2D and 3D power spectra, that can be retrieved as
-
-# 1D P(k)
-#k1D1      = Pk1.k1D
-#Pk1D1     = Pk1.Pk1D
-#Nmodes1D1 = Pk1.Nmodes1D
-
-#k1D2 = Pk2.k1D
-#Pk1D2 = Pk2.Pk1D
-#Nmodes1D2 = Pk2.Nmodes1D
-
-# 2D P(k)
-#kpar1     = Pk1.kpar
-#kper1     = Pk1.kper
-#Pk2D1     = Pk1.Pk2D
-#Nmodes2D1 = Pk1.Nmodes2D
-
-#kpar2 = Pk2.kpar
-#kper2 = Pk2.kper
-#Pk2D2 = Pk2.Pk2D
-#Nmodes2D2 = Pk2.Nmodes2D
-
-# 3D P(k)
-#k1       = Pk1.k3D
-#Pk01     = Pk1.Pk[:,0] #monopole
-#Pk21     = Pk1.Pk[:,1] #quadrupole
-#Pk41     = Pk1.Pk[:,2] #hexadecapole
-#Pkphase1 = Pk1.Pkphase #power spectrum of the phases
-#Nmodes1  = Pk1.Nmodes3D
-
-#k2 = Pk2.k3D
-#Pk02 = Pk2.Pk[:,0]
-#Pk22 = Pk2.Pk[:,1]
-#Pk42 = Pk2.Pk[:,2]
-#Pkphase2 = Pk2.Pkphase
-#Nmodes2 = Pk2.Nmodes3D
-
-
-#file = np.loadtxt("/data/jadame/nucome/0.0eV/output/test1024hybrid_pk007_delta.dat")
-
-#fig = plt.figure()
-#plt.plot(k1, Pk01/Pk02)
-#plt.xscale("log")
-#plt.xlabel(r"k [$h/Mpc$]")
-#plt.ylabel("P(k) gevolution / P(k) reference run") 
-#plt.ylim([0.96, 1.01])
-#fig.savefig("Pk0_"+snapshot_name+".pdf")
-#plt.close(fig)
-
-#fig = plt.figure()
-#plt.loglog(k1, Pk01)
-#plt.loglog(k2, Pk02)
-#plt.loglog(file[:,0], file[:,1]/file[:,0]**3*2*np.pi**2)
-#plt.xlabel(r"k [$h/Mpc$]")
-#plt.ylabel(r"P(k) [$Mpc^3/h^3$]")
-#fig.savefig("Pk0_"+snapshot_name+"_comparison.pdf")
